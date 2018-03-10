afbcb7d24df0fbada6f88ae1987a835ef5e719fc
Bobzhang <bobzhang1994@outlook.com>
2018-03-10 13:21:25 +0800

    创建了zhaopin.controller/zhaopin.hibernate.entity包结构

--- src/main/java/zhaopin/controller/MainController.java
+++ src/main/java/zhaopin/controller/MainController.java
@@ -1,4 +1,4 @@
-package personal;
+package zhaopin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import personal.User;
-import personal.UserRepository;
+import zhaopin.hibernate.entity.User;
+import zhaopin.hibernate.UserRepository;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
